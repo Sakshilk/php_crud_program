4e97ca21fb7f9965d7bf4ebcd17a1adc5d9f8bfb
Sakshilk <sakshilk406@gmail.com>
2026-05-15 21:41:41 +0530

    added a new file which calls the account functions

---
+++ account_functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function create_account() {
+  printf("Creating Your Acccount. Please enter the required details:").PHP_EOL;
+
+}
+
+function retrieve_account() {
+  printf("To retrieve your account, please enter your account number: ");
+}
+
+function update_account(){
+  printf("---Update Account---");
+}
+
+function delete_account(){
+  printf("Delete account:");
+}
+
+function account_queries(){
+  print("--Account Queries--");
+}
+
+
+
+?>
