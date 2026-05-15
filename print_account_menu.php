e4e9244c6c9fe0d587bc27410608abb0dd4e9e89
Sakshilk <sakshilk406@gmail.com>
2026-05-15 21:42:23 +0530

    added the requirement of account_functions.php file

--- print_account_menu.php
+++ print_account_menu.php
@@ -1,16 +1,48 @@
 <?php
+require "account_functions.php";
+
 function print_account_menu()
 {
   print "1. Create Account ".PHP_EOL;
   print "2. Retrieve Account ".PHP_EOL;
   print "3. Update Account ".PHP_EOL;
   print "4. Delete Account ".PHP_EOL;
-  print "5. Create Account ".PHP_EOL;
+  print "5. Queries ".PHP_EOL;
   print "0. Exit ".PHP_EOL;
-  print "00. Back to Main Menu".PHP_EOL;
+  print "8. Back to Main Menu".PHP_EOL;
+  
+  $account_choice = readline("Enter your choice: ").PHP_EOL;
+  
+  switch($account_choice) {
+    case 1:
+      create_account();
+      break;
+    case 2:
+      retrieve_account();
+      break;
+    case 3:
+      update_account();
+      break;
+    case 4:
+      delete_account();
+      break;
+    case 5:
+      acccount_queries();
+      break;
+    case 0:
+      exit(0);
+      break;
+    case 8:
+      exit_function();
+      break;
+    default:
+      printf("Please enter the correct choice!");
+      break;
+  }
+  
 }
 
-
+?>
 
 
 
