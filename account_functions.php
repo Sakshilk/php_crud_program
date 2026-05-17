e468df10af713c7a3b68f63fc4fce1b7ac4330d6
Sakshilk <sakshilk406@gmail.com>
2026-05-17 06:34:13 +0530

    users will be able to enter some input for contact details

--- account_functions.php
+++ account_functions.php
@@ -1,8 +1,37 @@
 <?php
+create_account();
+/**
+ * PARAMAETERS: 
+ * account_id => integer
+ * account_name => string 
+ * industry_type => string
+ * annual_revenue => float 
+ * employee_count => integer
+ * website => string 
+ * phone_number => integer
+ * billing_address => strings and integers 
+ * is_active => boolean 
+ */
 
 function create_account() {
-  printf("Creating Your Acccount. Please enter the required details:").PHP_EOL;
+  printf("Creating Your Acccount. Please enter the required details: \n").PHP_EOL;
+  $account_id = readline("Enter your account ID:").PHP_EOL;
+  $account_name = readline("Account Name: ").PHP_EOL;
+  $industry_type = readline("Industry Type: ").PHP_EOL;
+  $annual_revenue = readline("Annual Reveue ").PHP_EOL;
+  $employee_count = readline("Employee Count: ").PHP_EOL;
+  $website = readline("Account Website: ").PHP_EOL;
+  $phone_number = readline("Company contact number: ").PHP_EOL;
+  $account_file = fopen("$account_id.txt","w") or die("Error: Unable to create your file. Please try again later!");
+  fwrite($account_file,$account_id);
+  fwrite($account_file,$account_name);
+  fwrite($account_file,$industry_type);
+  fwrite($account_file,$annual_revenue);
+  fwrite($account_file,$employee_count);
+  fwrite($account_file,$website);
+  fwrite($account_file,$phone_number);
 
+  
 }
 
 function retrieve_account() {
--- main.php
+++ main.php
@@ -1,12 +1,14 @@
 <?php
 
 // files required 
-require "PrintAccountMenu.php";
-require "PrintContactMenu.php";
+require "print_account_menu.php";
+require "print_contact_menu.php";
+require "exit_function";
 
-
+//function call to print menu to the user 
 print_menu();
 
+//user's input choice is a global variable because i want to access the variable from other functions. Is there an alternate way?
 global $UserMainChoice;
 
 function print_menu()
@@ -15,20 +17,27 @@ function print_menu()
   print ("1.Accounts") . PHP_EOL;
   print ("2.Contacts") . PHP_EOL;
   print ("0.Exit") . PHP_EOL;
+
   $UserMainChoice = readline("Please enter your choice:") . PHP_EOL;
+
   switch ($UserMainChoice) {
+
     case 1:
       print_account_menu();
       break;
+
     case 2:
       print_contact_menu();
       break;
+
     case 3:
       echo "Exiting the program....";
       exit(0);
       break;
+
     default:
       echo "Please enter the correct choice!";
       print_menu();
   }
 }
+?>
